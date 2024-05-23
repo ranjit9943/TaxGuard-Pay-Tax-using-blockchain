2b31cb7f2db26c50d727b212363f485bcc952a2b
Ranjit Raj <ranjitraj9943@gmail.com>
2024-05-23 22:22:52 +0530

    Update firebaseConfig.js

--- taxpayweb/src/firebaseConfig.js
+++ taxpayweb/src/firebaseConfig.js
@@ -5,14 +5,8 @@ import { getFirestore } from 'firebase/firestore';
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBpUnG3tol7rHCv0uKVj1bMu_FwKm9Ru4Q",
-  authDomain: "taxchain2.firebaseapp.com",
-  databaseURL: "https://taxchain2-default-rtdb.firebaseio.com",
-  projectId: "taxchain2",
-  storageBucket: "taxchain2.appspot.com",
-  messagingSenderId: "166055096188",
-  appId: "1:166055096188:web:b474d98db75a0e5bdcbef9",
-  measurementId: "G-MHJQFC2DZP"
+// Your Firebase config
+  // enable authentication and firebase database
 };
 
 // Initialize Firebase
